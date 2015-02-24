87c8903c3168b2f0e5d6f6d37982a19529ef9e27
Jimmy Hedman <jimmy.hedman@southpole.se>
2015-02-24 23:37:00 +0100

    First revision.

--- reports/projects/admin.py
+++ reports/projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from projects.models import *
+
+admin.site.register(Project)
+admin.site.register(Observation)
+admin.site.register(Pipe)
+admin.site.register(Location)
+
--- reports/projects/models.py
+++ reports/projects/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+	name = models.CharField(max_length = 200)
+	client = models.CharField(max_length = 200)
+	operator = models.CharField(max_length = 200)
+	address = models.CharField(max_length = 200)
+	start_time = models.DateField()
+	end_time = models.DateField()
+	status = models.CharField(max_length = 200)
+	datafolder = models.CharField(max_length = 200)
+
+class Location(models.Model):
+	address = models.CharField(max_length = 200)
+
+class Pipe(models.Model):
+	location = models.ForeignKey(Location)
+	pipe_dimension = models.IntegerField()
+	spillwater = models.BooleanField(default = False)
+	daywater = models.BooleanField(default = False)
+	upstream = models.BooleanField(default = False)
+	cleansed_before = models.BooleanField(default = False)
+	previously_inspected = models.BooleanField(default = False)
+
+class Observation(models.Model):
+	project = models.ForeignKey(Project)
+	pipe = models.ForeignKey(Pipe)
+	grade = models.CharField(max_length = 200)
+	distance = models.IntegerField()
+	comment = models.CharField(max_length = 200)
+	picture = models.CharField(max_length = 200)

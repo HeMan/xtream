53cece553728d5d009d98853695f32bf927b0c02
Jimmy Hedman <jimmy.hedman@southpole.se>
2015-03-02 20:52:49 +0100

    Initial checkin of view.

--- reports/reports/urls.py
+++ reports/reports/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'reports.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^blog/', include('blog.urls')),
+    url(r'^projects/', include('projects.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )

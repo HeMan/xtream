a19b6c50abfb77f9f3bb3226b899737a9c89c460
Jimmy Hedman <jimmy.hedman@southpole.se>
2015-03-03 16:03:53 +0100

    First subpage.

--- reports/projects/urls.py
+++ reports/projects/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 from projects import views
 
-urlpatterns = patterns('',
-                           url(r'^$', views.index, name='index'),
+urlpatterns = patterns('', 
+                       url(r'^$', views.index, name='index'),
+                       url(r'^(?P<project_id>\d+)/$', views.project, name='project'),
                       )
--- reports/projects/views.py
+++ reports/projects/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from projects.models import Project
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    project_list = Project.objects.all()
+    print(project_list)
+    output = ', '.join([p.name for p in project_list])
+
+    return HttpResponse(output)
+    #return HttpResponse("Hello, world. You're at the project index.")
+
+def project(request, project_id):
+    return HttpResponse("This is your report %s" % (project_id))

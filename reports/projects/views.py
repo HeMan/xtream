83511b27acc503e86259f79e083ab3b8911965d3
Jimmy Hedman <jimmy.hedman@southpole.se>
2015-03-11 20:43:28 +0100

    Shows all projects and let you select project.

--- reports/projects/views.py
+++ reports/projects/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-from projects.models import Project
+from django.template import add_to_builtins
+
+add_to_builtins('ember.templatetags.ember')
+
+from projects.models import *
 
 def index(request):
     project_list = Project.objects.all()
-    print(project_list)
-    output = ', '.join([p.name for p in project_list])
 
-    return HttpResponse(output)
-    #return HttpResponse("Hello, world. You're at the project index.")
+    template = loader.get_template('projects/index.html')
+    context = RequestContext(request, {
+                'project_list': project_list,
+            })
+    return HttpResponse(template.render(context))
 
 def project(request, project_id):
-    return HttpResponse("This is your report %s" % (project_id))
+    project = Project.objects.get(pk=project_id)
+    observations = project.observation_set.all()
+    print(project.name)
+    template = loader.get_template('projects/project.html')
+    context = RequestContext(request, {
+                'project': project,
+                'observations': observations,
+            })
+    return HttpResponse(template.render(context))

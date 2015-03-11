30be10b087bca1027ea9c6192cfac2fc85fe13e2
Jimmy Hedman <jimmy.hedman@southpole.se>
2015-03-11 20:42:59 +0100

    Added namespace.

--- reports/reports/urls.py
+++ reports/reports/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'reports.views.home', name='home'),
     #url(r'^blog/', include('blog.urls')),
-    url(r'^projects/', include('projects.urls')),
+    url(r'^projects/', include('projects.urls', namespace="projects")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
